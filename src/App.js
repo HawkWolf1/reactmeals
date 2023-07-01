205b4d4f52c6f388b530cb70165e31fb956336f9
HawkWolf1 <singhavinash9fg@gmail.com>
2023-07-01 18:30:07 +0530

    Restaurant summary added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import Card from './Header/Card.js'
+import Overlaycard from './Header/Overlaycard.js';
 
 
 function App() {
   return (
     <div>
       <Card/>
-      
+      <Overlaycard />
     </div>
   );
 }
---
+++ src/Header/Overlaycard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Overlaycard.css';
+
+function OverlayCard() {
+    return (
+      <div className="overlay-card">
+        <div className="overlay-card-content">
+          <h4>DELICIOUS FOOD DELIVERED TO YOU</h4>
+          <p>
+            Choose your favorite meals from our broad selection of favorite meals.
+            All our meals are cooked using high-quality ingredients and experienced chefs.
+          </p>
+        </div>
+      </div>
+    );
+  }
+
+export default OverlayCard;
